4c423223f46dbf7aa79afd81e9d83fe86930d1bb
Sarah Keashly <sarahejohnson89@gmail.com>
2023-10-09 15:23:47 -0700

    Add in Edit functionality by creating put axios request and updating the edit button to access the product id

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,8 +4,8 @@
     <!-- Button to open the modal -->
     <button @click="openModal">Add Product</button>
 
-    <!-- Modal form for adding new product -->
-    <div v-if="isModalOpen" class="modal">
+        <!-- Modal form for adding new product -->
+        <div v-if="isModalOpen" class="modal">
       <div class="modal-content">
         <span class="close" @click="closeModal">&times;</span>
 
@@ -90,7 +90,7 @@
         <td>{{ product.startDate}}</td>
         <td>{{ product.methodology}}</td>
         <td>{{ product.location}}</td>
-        <!-- <td><button @click="editProduct">Edit</button></td> -->
+        <td><button @click="openEditModal(product.productId)">Edit</button></td>
         <!-- ... other cells ... -->
       </tr>
       <tr><br></tr>
@@ -99,6 +99,51 @@
         <td>{{ count }}</td>
       </tr>
     </table>
+
+    <!-- Modal form for editing a product -->
+    <div v-if="isEditModalOpen" class="modal">
+      <div class="modal-content">
+        <span class="close" @click="closeModal">&times;</span>
+        <h2>Edit Product</h2>
+        <!-- Form for editing a product -->
+        <form @submit.prevent="saveEdit">
+          <label for="productName">Product Name:</label>
+          <input type="text" v-model="editedProduct.productName">
+
+          <label for="scrumMasterName">Scrum Master:</label>
+          <input type="text" v-model="editedProduct.scrumMasterName">
+
+          <label for="productOwnerName">Product Owner:</label>
+          <input type="text" v-model="editedProduct.productOwnerName" >
+
+          <label for="developer1">Developer 1:</label>
+          <input type="text" v-model="editedProduct.developers[0]" placeholder="Developer 1">
+
+          <label for="developer2">Developer 2:</label>
+          <input type="text" v-model="editedProduct.developers[1]" placeholder="Developer 2">
+
+          <label for="developer3">Developer 3:</label>
+          <input type="text" v-model="editedProduct.developers[2]" placeholder="Developer 3">
+
+          <label for="developer4">Developer 4:</label>
+          <input type="text" v-model="editedProduct.developers[3]" placeholder="Developer 4">
+
+          <label for="developer5">Developer 5:</label>
+          <input type="text" v-model="editedProduct.developers[4]" placeholder="Developer 5">
+
+          <label for="startDate">Start Date:</label>
+          <input type="date" v-model="editedProduct.startDate" >
+
+          <label for="methodology">Methodology:</label>
+          <select v-model="editedProduct.methodology" >
+            <option value="Agile">Agile</option>
+            <option value="Waterfall">Waterfall</option>
+          </select>
+
+          <button type="submit">Save</button>
+        </form>
+      </div>
+    </div>
   </div> 
 </template>
 
@@ -107,8 +152,18 @@ export default {
   data() {
     return {
       isModalOpen: false,
+      isEditModalOpen: false,
       newProduct: {
         productId: Math.random().toString(36).substr(2, 9),
+        productName: '',
+        scrumMasterName: '',
+        productOwnerName: '',
+        developers: [],
+        startDate: '',
+        methodology: 'Agile',
+      },
+      editedProduct: {
+        productId: '',
         productName: '',
         scrumMasterName: '',
         productOwnerName: '',
@@ -145,6 +200,17 @@ export default {
       console.log("open modal")
       this.isModalOpen = true;
     },
+    openEditModal(productId) {
+      console.log("open edit modal")
+
+      // Find the product in the array based on productId
+      const productToEdit = this.products.find(product => product.productId === productId);
+
+      // Set the editedProduct to the found product
+      this.editedProduct = { ...productToEdit };
+
+      this.isEditModalOpen = true;
+    },
     closeModal() {
       this.isModalOpen = false;
     },
@@ -157,32 +223,59 @@ export default {
     },
 
     addProduct() {
-  // Add validation logic here if needed
+    // Add validation logic here if needed
 
-  // Make a POST request to add the new product to the server
-  this.$axios.post('/api/products', this.newProduct)
-    .then(response => {
-      // Add the new product to the local array
-      this.products.push(response.data);
+    // Make a POST request to add the new product to the server
+    this.$axios.post('/api/products', this.newProduct)
+      .then(response => {
+        // Add the new product to the local array
+        this.products.push(response.data);
+
+        // Close the modal
+        this.closeModal();
+
+        // Optionally, you can clear the form fields
+        this.newProduct = {
+          productName: '',
+          scrumMasterName: '',
+          productOwnerName: '',
+          developers: [],
+          startDate: '',
+          methodology: 'Agile',
+        };
+      })
+      .catch(error => {
+        console.error('Error adding product:', error);
+      });
+    },
+
+    saveEdit(){
+      console.log("saveEdit");
+      // Add validation logic here if needed
+
+      // Make a PUT request to edit the product on the server
+      this.$axios.put(`/api/products/${this.editedProduct.productId}`, this.editedProduct)
+      .then(response => {
+      // Update the local array with the updated product
+      const index = this.products.findIndex(p => p.productId === this.editedProduct.productId);
+
+      if (index !== -1) {
+        this.$set(this.products, index, response.data);
+      }
 
       // Close the modal
-      this.closeModal();
+      this.isEditModalOpen = false;
+      this.editedProduct = null;
 
-      // Optionally, you can clear the form fields
-      this.newProduct = {
-        productName: '',
-        scrumMasterName: '',
-        productOwnerName: '',
-        developers: [],
-        startDate: '',
-        methodology: 'Agile',
-      };
-    })
-    .catch(error => {
-      console.error('Error adding product:', error);
-    });
-},
+      })
+      .catch(error => {
+        console.error('Error editing product:', error);
+      });
+    },
     
+
+    
+      
   },
 
 };
