03058de1cbf48f1c34583956cdb9667032d966e5
Sarah Keashly <sarahejohnson89@gmail.com>
2023-10-09 11:46:48 -0700

    Create json file, iinstall fs module and update endpoints and axios request to make adding product persistent when there is a page refresh

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const bodyParser = require('body-parser');
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 const cors = require('cors');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
-
 app.use(bodyParser.json());
 
 // Swagger definition
@@ -33,75 +33,17 @@ const swaggerSpec = swaggerJSDoc(options);
 // Serve Swagger documentation
 app.use('/api/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
-// ... existing routes ...
 
-// Sample data
-const products = [
-  {
-    "productId": "c7524f22-5748-4122-babd-2d56171b810c",
-    "productName": "Rustic Steel Gloves",
-    "productOwnerName": "Randy Gerhold",
-    "developers": [
-      "Mrs. Madeline Littel",
-      "Dr. Mercedes Howe",
-      "Jo Goyette",
-      "Jesus Lebsack",
-      "Julius Anderson"
-    ],
-    "scrumMasterName": "Guadalupe Morissette",
-    "startDate": "2023-01-16",
-    "methodology": "Waterfall",
-    "location": "https://github.com/bcgov/sequi"
-  },
-  {
-    "productId": "5339b0b0-7a5d-4b59-b16e-cbec40775acf",
-    "productName": "Sleek Plastic Computer",
-    "productOwnerName": "Kevin Stoltenberg",
-    "developers": [
-      "Jill Windler",
-      "Emmett Legros",
-      "Omar Pfeffer DVM",
-      "Miranda Rohan",
-      "Miss Eva Kassulke"
-    ],
-    "scrumMasterName": "Devin Jacobs",
-    "startDate": "2023-01-09",
-    "methodology": "Waterfall",
-    "location": "https://github.com/bcgov/magnam"
-  },
-  {
-    "productId": "27036f08-0998-4633-a0f4-e11d7996a492",
-    "productName": "Tasty Wooden Ball",
-    "productOwnerName": "Howard Corwin",
-    "developers": [
-      "Nathaniel Jacobi Jr.",
-      "Gilbert Littel",
-      "Sabrina Jones",
-      "Noah Schultz",
-      "Michelle Schowalter"
-    ],
-    "scrumMasterName": "Mr. Freda Ratke",
-    "startDate": "2023-03-10",
-    "methodology": "Waterfall",
-    "location": "https://github.com/bcgov/necessitatibus"
-  },
-  {
-    "productId": "2f5d178c-3f18-4b24-958d-7385af0fff38",
-    "productName": "Refined Frozen Tuna",
-    "productOwnerName": "Devin Larkin",
-    "developers": [
-      "Raquel Johns",
-      "Terrence Ernser",
-      "Judith Bernhard",
-      "Jeffery Conn",
-      "Antoinette Borer"
-    ],
-    "scrumMasterName": "Eric Kozey",
-    "startDate": "2022-10-25",
-    "methodology": "Agile",
-    "location": "https://github.com/bcgov/inventore"
-  },
-];
+// Read the products from the JSON file
+const productsFilePath = './data.json';
+let products = [];
+
+try {
+  const productsData = fs.readFileSync(productsFilePath, 'utf-8');
+  products = JSON.parse(productsData);
+} catch (error) {
+  console.error('Error reading products file:', error);
+}
 
 // Health endpoint
 app.get('/api/health', (req, res) => {
@@ -113,21 +55,78 @@ app.get('/api/products', (req, res) => {
   res.json(products);
 });
 
+// // Add a new product
+// app.post('/api/products', (req, res) => {
+//   const newProduct = req.body;
+//   // Add validation and generate product number here
+//   products.push(newProduct);
+//   saveDataToFile();
+//   res.status(201).json(newProduct);
+// });
+
 // Add a new product
 app.post('/api/products', (req, res) => {
   const newProduct = req.body;
-  // Add validation and generate product number here
   products.push(newProduct);
+
+  // Save updated data to the JSON file
+  fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+
   res.status(201).json(newProduct);
 });
+
+// Edit a product
+// app.put('/api/products/:productId', (req, res) => {
+//   const productId = req.params.productId;
+//   const updatedProduct = req.body;
+//   // Add validation and update logic here
+//   const index = products.findIndex(p => p.productId === productId);
+//   if (index !== -1) {
+//     products[index] = updatedProduct;
+//     saveDataToFile();
+//     res.status(200).json(updatedProduct);
+//   } else {
+//     res.status(404).send('Product not found');
+//   }
+// });
 
 // Edit a product
 app.put('/api/products/:productId', (req, res) => {
   const productId = req.params.productId;
   const updatedProduct = req.body;
-  // Add validation and update logic here
-  res.status(200).json(updatedProduct);
+
+  // Find the index of the product to update
+  const index = products.findIndex(p => p.productId === productId);
+
+  if (index !== -1) {
+    // Update the product
+    products[index] = updatedProduct;
+
+    // Save updated data to the JSON file
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+
+    res.status(200).json(updatedProduct);
+  } else {
+    res.status(404).send('Product not found');
+  }
 });
+
+function saveDataToFile() {
+  fs.writeFileSync('data.json', JSON.stringify(products, null, 2), 'utf-8');
+}
+
+// Load data from file on server start
+function loadDataFromFile() {
+  try {
+    const data = fs.readFileSync('data.json', 'utf-8');
+    products = JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading data from file:', error.message);
+  }
+}
+
+// Load initial data from file on server start
+loadDataFromFile();
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
